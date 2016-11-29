b25e4f5a1a25940c4754b5eebd44a7ed53fbde87
154290629@qq.com <154290629@qq.com>
2016-11-30 01:08:55 +0800

    bak

--- app/module_assetsmanage/controllers/CollectionController.js
+++ app/module_assetsmanage/controllers/CollectionController.js
@@ -2,15 +2,37 @@
 
 /* Controllers */
 angular.module("pu.assetsmanage.controllers")
-    .controller('CollectionController',function ($scope, $rootScope, $state,$stateParams, toaster, $uibModal,CollectionService,LoanQueryService) {
+    .controller('CollectionController',function ($scope, $rootScope, $state,$stateParams, toaster, $uibModal,CollectionService,LoanQueryService,SysDictService) {
         $scope.initCollectionData = function(){
             $scope.procInstId = $stateParams.procInstId;
             $scope.taskId = $stateParams.taskId;
             $scope.businessKey = $stateParams.businessKey;
             $scope.appId = $stateParams.appId;
             $scope.workflowKey = $stateParams.workflowKey;
+            $scope.applyVo = {};
+            $scope.doInitApplyEdit($stateParams.appId);
             $scope.task = LoanQueryService.getTaskByTaskId($stateParams.taskId,$stateParams.workflowKey).$object;
             $scope.baseInfoVo = LoanQueryService.getLoanCustApplyInfo($scope.appId).$object;
-        }
+            $scope.collectionStatusList = SysDictService.queryDictDataByTypeCode("cszt").$object;
+            $scope.overdueReasonList  =  SysDictService.queryDictDataByTypeCode("yqyy").$object;
+            $scope.recoverStatusList = SysDictService.queryDictDataByTypeCode("sczt").$object;
+            $scope.refundStatusList = SysDictService.queryDictDataByTypeCode("thzt").$object;
+            $scope.disposeStatusList = SysDictService.queryDictDataByTypeCode("czzt").$object;
+            $scope.lawsuitStatusList = SysDictService.queryDictDataByTypeCode("sszt").$object;
+        };
+        $scope.initPhoneCollection = function(){
+            $scope.initCollectionData();
+        };
+        $scope.saveCollectionLog =function(){
+            CollectionService.saveCollectionLog($scope.taskId,$scope.applyVo).then(function(response){
+                toaster.pop('success', '操作提醒', '保存日志成功 ');
+            })
+        };
+        $scope.getImportantCollectionLog = function(){
+            $scope.importanCollectionLogList = CollectionService.getImportanCollectionLogInfo($scope.appId).$object;
+        };
+        $scope.getCollectionLog = function(taskType){
+            $scope.collectionLogList = CollectionService.getCollectionLogInfo($scope.appId,taskType).$object;
+        };
     })
 ;
