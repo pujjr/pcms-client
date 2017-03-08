c281c33a7453391ae19d74d8f0f423ba5bbf6a23
dedp1984 <154290629@qq.com>
2017-03-08 08:54:27 +0800

    bak

--- app/version.js
+++ app/version.js
@@ -1,7 +1,7 @@
 angular.module('app')
     .run(
     ['$rootScope', function ($rootScope) {
-            $rootScope.version = '1.0.15.20170307'
+            $rootScope.version = '1.0.16.20170307'
         }
     ])
 
