c396e1459a44339656c490b9901eb5d1916046f7
dedp1984 <154290629@qq.com>
2017-03-15 09:46:50 +0800

    修复查询征信返回取OSSKEY报错

--- app/version.js
+++ app/version.js
@@ -1,7 +1,7 @@
 angular.module('app')
     .run(
     ['$rootScope', function ($rootScope) {
-            $rootScope.version = '1.0.23.20170314'
+            $rootScope.version = '1.0.24.20170314'
         }
     ])
 
